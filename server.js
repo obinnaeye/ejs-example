61e82c39c46035a12e04c0e499703f20d765d6b7
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-04-02 15:38:52 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+//const ejs = require("ejs");
+
+const app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+    res.render('pages/index', {
+        drinks: drinks,
+        tagline: tagline
+    });
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(process.env.PORT || 8080);
+console.log(`${process.env.PORT} is the magic port`);
